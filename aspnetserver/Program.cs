466a8668ed3f23f2a7a9b9767011770834b036e9
Brian Freestone <briandfreestone@gmail.com>
2022-05-30 09:59:18 -0600

    fixed correct repo

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -3,6 +3,19 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000", "https://kind-water-07b064810.1.azurestaticapps.net");
+        });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
